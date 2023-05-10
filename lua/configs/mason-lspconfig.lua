1333e4e6f4faf8f4c9630c7018268262647c96b7
arakkk <bdkouiti5013@gmail.com>
2023-05-11 07:14:18 +0900

    update

---
+++ lua/configs/mason-lspconfig.lua
@@ -0,0 +1,13 @@
+require("mason-lspconfig").setup({
+  ensure_installed = {
+    -- "sumneko_lua",
+    "pyright",
+    -- "html",
+    -- "texlab",
+    -- "lua-language-server",
+    -- "haskell-language-server",
+    -- "black",
+    -- "ltex-ls",
+    -- "stylua",
+  }
+})
