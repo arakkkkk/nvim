c569eb3ac25a73399f10ea07a61786fc9b7ae6b3
arakkk <bdkouiti5013@gmail.com>
2023-11-15 23:21:16 +0900

    update

--- lua/configs/nvim-pasta.lua
+++ lua/configs/nvim-pasta.lua
@@ -1,8 +1,2 @@
 vim.keymap.set({ "n", "x" }, "p", require("pasta.mapping").p)
 vim.keymap.set({ "n", "x" }, "P", require("pasta.mapping").P)
-
--- This is the default. You can omit `setup` call if you don't want to change this.
-require("pasta").config.next_key = vim.keycode("<C-n>")
-require("pasta").config.prev_key = vim.keycode("<C-p>")
-require("pasta").config.indent_key = vim.keycode(",")
-require("pasta").config.indent_fix = true
